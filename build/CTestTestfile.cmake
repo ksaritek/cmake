212295698ee469c42f015117f85af6858384d8b6
ksaritek <koraysariteke@gmail.com>
2023-03-24 02:22:27 +0100

    step 2

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/koray.sariteke/git/personal/cmake
-# Build directory: /Users/koray.sariteke/git/personal/cmake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
